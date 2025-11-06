ce76dae18bb29d19782359d6d96b8404de93af9b
Kevin Jayne <kevin.jayne@injectivelabs.org>
2025-11-05 23:00:07 -0500

    psql

--- scripts/k3s/09-psql.sh
+++ scripts/k3s/09-psql.sh
@@ -2,14 +2,17 @@
 
 set -e
 
-kubectl create namespace psql || true
+PSQL_NAMESPACE="psql"
 
 # Helm repo setup
 helm repo add bitnami https://charts.bitnami.com/bitnami
 helm repo update
 
-# Install PostgreSQL using Helm with values file
+# Install PostgreSQL using Helm with flags
 helm upgrade --install psql bitnami/postgresql \
-  --namespace psql \
+  --namespace ${PSQL_NAMESPACE} \
   --create-namespace \
-  -f ./services/psql/psql-values.yaml
+  --set primary.persistence.storageClass=local-path \
+  --set primary.persistence.enabled=true \
+  --set primary.persistence.size=10Gi \
+  --set volumePermissions.enabled=true
