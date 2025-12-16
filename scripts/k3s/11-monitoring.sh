fe88afbe61599e7d17a2800208addcc77a38ae0a
Kevin Jayne <kevin.jayne@injectivelabs.org>
2025-12-16 03:33:20 -0500

    grafana-json

--- scripts/k3s/11-monitoring.sh
+++ scripts/k3s/11-monitoring.sh
@@ -26,3 +26,18 @@ helm upgrade --install monitoring-stack prometheus-community/kube-prometheus-sta
   --create-namespace \
   --values ./helm/monitoring/values.yaml
 
+# Install Loki for logging
+helm repo add grafana https://grafana.github.io/helm-charts
+helm repo update
+
+# Install Loki for logging
+# Promtail (enabled below) automatically discovers and collects logs from all pods in all namespaces
+# It runs as a DaemonSet and uses Kubernetes service discovery to find pods
+helm upgrade --install loki grafana/loki-stack \
+  --namespace "monitoring" \
+  --set loki.persistence.enabled=true \
+  --set loki.persistence.storageClassName=local-path \
+  --set loki.persistence.size=20Gi \
+  --set promtail.enabled=true \
+  --set loki.config.limits_config.retention_period=192h
+
