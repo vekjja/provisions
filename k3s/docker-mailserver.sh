37f9d086775968c9cddeb0126e6b2b28a2bfc775
Kevin Jayne <kevin.jayne@injectivelabs.org>
2025-12-24 14:04:18 -0500

    mail-dns-records

--- k3s/docker-mailserver.sh
+++ k3s/docker-mailserver.sh
@@ -43,9 +43,52 @@ helm install docker-mailserver docker-mailserver/docker-mailserver \
 # Initial Setup
 # ------------
 #  You have two minutes to setup a first email account after the initial deployment.
-
 kubectl exec -it --namespace mail deploy/docker-mailserver -- bash
 setup email add user@example.com password
+
+# Generate DKIM key
+kubectl exec -n mail deploy/docker-mailserver -- setup config dkim
+
+# Capture DKIM TXT value (flattened) into env var for reuse
+DKIM_TXT=$(kubectl exec -n mail deploy/docker-mailserver -- sh -c "cat /tmp/docker-mailserver/opendkim/keys/mail.livingroom.cloud/mail.txt" \
+  | awk -F'"' 'BEGIN{ORS="";}{for(i=2;i<NF;i+=2)printf "%s",$i}END{print "";}')
+echo "DKIM TXT: ${DKIM_TXT}"
+
+# Update DNS records with DKIM key (keep all existing endpoints together)
+cat <<EOF | kubectl apply -f -
+---
+apiVersion: externaldns.k8s.io/v1alpha1
+kind: DNSEndpoint
+metadata:
+  name: livingroom-cloud-mail-records
+  namespace: external-dns
+spec:
+  endpoints:
+  - dnsName: mail.livingroom.cloud
+    recordType: A
+    recordTTL: 300
+    targets: [ "174.44.105.210" ]
+  - dnsName: mail.livingroom.cloud
+    recordType: MX
+    recordTTL: 3600
+    targets: [ "10 mail.livingroom.cloud." ]
+  - dnsName: mail.livingroom.cloud
+    recordType: TXT
+    recordTTL: 300
+    targets: [ "v=spf1 a:mail.livingroom.cloud ip4:174.44.105.210 -all" ]
+  - dnsName: _dmarc.mail.livingroom.cloud
+    recordType: TXT
+    recordTTL: 300
+    targets: [ "v=DMARC1; p=none; rua=mailto:dmarc-reports@livingroom.cloud; pct=100" ]
+  - dnsName: mail._domainkey.mail.livingroom.cloud
+    recordType: TXT
+    recordTTL: 300
+    targets: [ "${DKIM_TXT}" ]
+---
+EOF
+
+# Show DKIM TXT record
+dig TXT mail._domainkey.mail.livingroom.cloud +short
 
 # Test the email setup
 kubectl exec -n mail deploy/docker-mailserver -- swaks --to seemywings@gmail.com --from admin@livingroom.cloud --server 127.0.0.1 --auth LOGIN --auth-user admin@livingroom.cloud --auth-password 'admin' --tls --port 587
